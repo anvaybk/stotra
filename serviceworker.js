937fc692a246562219b84097b2e84d9328322bc7
Anvay Kulkarni <anvaybkgtp@gmail.com>
2026-05-31 16:04:09 +0530

    New version 1.0.0.6 added

--- serviceworker.js
+++ serviceworker.js
@@ -2,9 +2,9 @@
 // NITYA STOTRA SERVICE WORKER
 // ===============================
 
-// Last updated: 29-05-2026
+// Last updated: 31-05-2026
 
-const VERSION = '29052026-1005';
+const VERSION = '29052026-1006';
 const CACHE_NAME = `nityastotra-${VERSION}`;
 
 // Detect base path dynamically
